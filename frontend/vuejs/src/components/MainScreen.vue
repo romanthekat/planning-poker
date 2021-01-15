44746b5e61864e6d2f89521bee26a30f42a8de81
Roman Garanin <git@romangaranin.net>
2021-01-15 23:06:08 +0300

    feat: FE very basic styling, 10% of prepare design

--- frontend/vuejs/src/components/MainScreen.vue
+++ frontend/vuejs/src/components/MainScreen.vue
@@ -1,247 +1,252 @@
 <template>
-    <div class="main">
-        <template v-if="!sessionFound()">
-            <button v-on:click="createSession">Create Session</button>
-            <label>
-                or join by id:
-                <input v-model.lazy="sessionId">
-            </label>
-        </template>
-        <template v-if="sessionFound() && userId === ''">
-            <label>
-                Type name:
-                <input v-model="name">
-            </label>
-            <button v-on:click="joinSession">Join Session</button>
-        </template>
-        <template v-if="sessionFound() && userId !== '' && session === null">
-            <label>
-                Type name:
-                <input v-model="name">
-            </label>
-            <button v-on:click="joinSession">Join Session</button>
-        </template>
-        <template v-if="sessionFound() && userId !== '' && session !== null">
-            <button class='vote-btn' v-on:click="voteInSession(1)">1</button>
-            <button class='vote-btn' v-on:click="voteInSession(2)">2</button>
-            <button class='vote-btn' v-on:click="voteInSession(3)">3</button>
-            <button class='vote-btn' v-on:click="voteInSession(5)">5</button>
-            <button class='vote-btn' v-on:click="voteInSession(8)">8</button>
-            <button class='vote-btn' v-on:click="voteInSession(13)">13</button>
-            <button class='vote-btn' v-on:click="voteInSession(20)">20</button>
+  <div class="main">
+    <img class="icon" src="@/assets/icon.png">
+    <template v-if="!sessionFound()">
+      <div class="create-session-container">
+        <button class="button create-button" v-on:click="createSession">
+          create
+        </button>
+        <label>
+          <div class="text">or join by id:</div>
+          <input class="session-id-input" v-model.lazy="sessionId">
+        </label>
+      </div>
+    </template>
+    <template v-if="sessionFound() && userId === ''">
+      <label>
+        Type name:
+        <input v-model="name">
+      </label>
+      <button class='button' v-on:click="joinSession">Join Session</button>
+    </template>
+    <template v-if="sessionFound() && userId !== '' && session === null">
+      <label>
+        Type name:
+        <input v-model="name">
+      </label>
+      <button v-on:click="joinSession">Join Session</button>
+    </template>
+    <template v-if="sessionFound() && userId !== '' && session !== null">
+      <button class='button vote-button' v-on:click="voteInSession(1)">1</button>
+      <button class='button vote-button' v-on:click="voteInSession(2)">2</button>
+      <button class='button vote-button' v-on:click="voteInSession(3)">3</button>
+      <button class='button vote-button' v-on:click="voteInSession(5)">5</button>
+      <button class='button vote-button' v-on:click="voteInSession(8)">8</button>
+      <button class='button vote-button' v-on:click="voteInSession(13)">13</button>
+      <button class='button vote-button' v-on:click="voteInSession(20)">20</button>
 
-            <br>
-            <button class='clear-btn' v-on:click="clearVotes">Clear votes</button>
-            <button class='show-votes-btn' v-on:click="showVotes">Show votes</button>
+      <br>
+      <button class='button clear-button' v-on:click="clearVotes">Clear votes</button>
+      <button class='button show-votes-button' v-on:click="showVotes">Show votes</button>
 
-            <template v-if="session.votes_info !== null && session.votes_info !== undefined">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Vote</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-bind:class="{ currentUser: vote.is_current_user}"
-                        v-for="vote in session.votes_info" :key="vote.name">
-                        <td>{{vote.name}}</td>
-                        <template v-if="vote.is_voted">
-                            <template v-if="vote.vote !== null">
-                                <td>{{vote.vote}}</td>
-                            </template>
-                            <template v-else>
-                                <td>Voted</td>
-                            </template>
-                        </template>
-                        <template v-else>
-                            <td>No vote</td>
-                        </template>
-                    </tr>
-                    </tbody>
-                </table>
+      <template v-if="session.votes_info !== null && session.votes_info !== undefined">
+        <table>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Vote</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-bind:class="{ currentUser: vote.is_current_user}"
+              v-for="vote in session.votes_info" :key="vote.name">
+            <td>{{ vote.name }}</td>
+            <template v-if="vote.is_voted">
+              <template v-if="vote.vote !== null">
+                <td>{{ vote.vote }}</td>
+              </template>
+              <template v-else>
+                <td>Voted</td>
+              </template>
             </template>
-        </template>
-        <template v-if="session !== null && session.votes_hidden === false">
-            <br>
-            <strong>Average:</strong><label> {{averageVote()}}</label>
-        </template>
-    </div>
+            <template v-else>
+              <td>No vote</td>
+            </template>
+          </tr>
+          </tbody>
+        </table>
+      </template>
+    </template>
+    <template v-if="session !== null && session.votes_hidden === false">
+      <br>
+      <strong>Average:</strong><label> {{ averageVote() }}</label>
+    </template>
+  </div>
 </template>
 
 <script>
-    import axios from "axios";
+import axios from "axios";
 
-    export default {
-        name: 'MainScreen',
-        props: {
-            backendUrl: String,
-            sessionId: Number,
-        },
-        data() {
-            return {
-                name: '',
-                userId: '',
-                timer: '',
-                session: {},
-                vote: 0.0
-            }
-        },
-
-        created() {
-            this.interval = setInterval(this.fetchSession, 1000)
-        },
-        beforeDestroy() {
-            clearInterval(this.interval)
-        },
-
-        methods: {
-            sessionFound() {
-                return this.sessionId != null && !isNaN(this.sessionId)
-            },
-            averageVote() {
-                let total = 0;
-                let count = 0;
-                for (var i = 0; i < this.session.votes_info.length; i++) {
-                    let voteInfo = this.session.votes_info[i];
-
-                    let vote = parseInt(voteInfo.vote);
-                    if (!isNaN(vote)) {
-                        total += vote;
-                        count++;
-                    }
-                }
-
-                if (count === 0) {
-                    return 0
-                }
-
-                return total / count;
-            },
-            fetchSession() {
-                if (this.userId !== '') {
-                    axios({
-                        method: 'get',
-                        baseURL: this.backendUrl,
-                        url: '/sessions/' + this.sessionId + '/get/' + this.userId,
-                    })
-                        .then(response => {
-                            this.session = response.data
-                        })
-                        .catch(error => {
-                            console.log(error);
-                        });
-                }
-            },
-            beforeDestroy() {
-                clearInterval(this.timer)
-            },
-
-            createSession() {
-                axios({
-                    method: 'post',
-                    baseURL: this.backendUrl,
-                    url: '/sessions',
-                })
-                    .then(response => {
-                        this.sessionId = response.data.id
-                        this.$router.push('/' + response.data.id)
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            joinSession() {
-                axios({
-                        method: 'post',
-                        baseURL: this.backendUrl,
-                        url: '/sessions/' + this.sessionId + '/join',
-                        data: JSON.stringify({name: this.name}),
-                    }
-                )
-                    .then(response => {
-                        this.userId = response.data.id
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            voteInSession(vote) {
-                this.vote = vote
-
-                axios({
-                        method: 'post',
-                        baseURL: this.backendUrl,
-                        url: '/sessions/' + this.sessionId + '/vote',
-                        data: JSON.stringify({user_id: this.userId, vote: parseFloat(vote)}),
-                    }
-                )
-                    .then(() => {
-                        this.fetchSession()
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            clearVotes() {
-                axios({
-                        method: 'post',
-                        baseURL: this.backendUrl,
-                        url: '/sessions/' + this.sessionId + '/clear',
-                        data: JSON.stringify({user_id: this.userId}),
-                    }
-                )
-                    .then(() => {
-                        this.fetchSession()
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            showVotes() {
-                axios({
-                        method: 'post',
-                        baseURL: this.backendUrl,
-                        url: '/sessions/' + this.sessionId + '/show',
-                        data: JSON.stringify({user_id: this.userId}),
-                    }
-                )
-                    .then(() => {
-                        this.fetchSession()
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            }
-        }
+export default {
+  name: 'MainScreen',
+  props: {
+    backendUrl: String,
+    sessionId: Number,
+  },
+  data() {
+    return {
+      name: '',
+      userId: '',
+      timer: '',
+      session: {},
+      vote: 0.0
     }
+  },
+
+  created() {
+    this.interval = setInterval(this.fetchSession, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+  },
+
+  methods: {
+    sessionFound() {
+      return this.sessionId != null && !isNaN(this.sessionId)
+    },
+    averageVote() {
+      let total = 0;
+      let count = 0;
+      for (var i = 0; i < this.session.votes_info.length; i++) {
+        let voteInfo = this.session.votes_info[i];
+
+        let vote = parseInt(voteInfo.vote);
+        if (!isNaN(vote)) {
+          total += vote;
+          count++;
+        }
+      }
+
+      if (count === 0) {
+        return 0
+      }
+
+      return total / count;
+    },
+    fetchSession() {
+      if (this.userId !== '') {
+        axios({
+          method: 'get',
+          baseURL: this.backendUrl,
+          url: '/sessions/' + this.sessionId + '/get/' + this.userId,
+        })
+            .then(response => {
+              this.session = response.data
+            })
+            .catch(error => {
+              console.log(error);
+            });
+      }
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
+    },
+
+    createSession() {
+      axios({
+        method: 'post',
+        baseURL: this.backendUrl,
+        url: '/sessions',
+      })
+          .then(response => {
+            this.sessionId = response.data.id
+            this.$router.push('/' + response.data.id)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    joinSession() {
+      axios({
+            method: 'post',
+            baseURL: this.backendUrl,
+            url: '/sessions/' + this.sessionId + '/join',
+            data: JSON.stringify({name: this.name}),
+          }
+      )
+          .then(response => {
+            this.userId = response.data.id
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    voteInSession(vote) {
+      this.vote = vote
+
+      axios({
+            method: 'post',
+            baseURL: this.backendUrl,
+            url: '/sessions/' + this.sessionId + '/vote',
+            data: JSON.stringify({user_id: this.userId, vote: parseFloat(vote)}),
+          }
+      )
+          .then(() => {
+            this.fetchSession()
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    clearVotes() {
+      axios({
+            method: 'post',
+            baseURL: this.backendUrl,
+            url: '/sessions/' + this.sessionId + '/clear',
+            data: JSON.stringify({user_id: this.userId}),
+          }
+      )
+          .then(() => {
+            this.fetchSession()
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    showVotes() {
+      axios({
+            method: 'post',
+            baseURL: this.backendUrl,
+            url: '/sessions/' + this.sessionId + '/show',
+            data: JSON.stringify({user_id: this.userId}),
+          }
+      )
+          .then(() => {
+            this.fetchSession()
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .currentUser {
-        font-weight: bold;
-    }
+.currentUser {
+  font-weight: bold;
+}
 
-    h3 {
-        margin: 40px 0 0;
-    }
+h3 {
+  margin: 40px 0 0;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-    a {
-        color: #42b983;
-    }
+a {
+  color: #42b983;
+}
 
-    table {
-        align: left
-    }
+table {
+  align: left
+}
 </style>
