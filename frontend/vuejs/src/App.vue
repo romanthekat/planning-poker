b5af9609c3ce7eac1cc48247ed67adb5bec295c6
Roman Garanin <evilkhaoskat@gmail.com>
2021-02-06 17:15:54 +0300

    feat: FE new style for first page

--- frontend/vuejs/src/App.vue
+++ frontend/vuejs/src/App.vue
@@ -21,20 +21,12 @@ export default {
     }
   },
   created() {
-    document.title = "=^_^= scrum planning poker";
+    document.title = "=^_^= scrum planning poker/planner cat";
   }
 }
 </script>
 
 <style>
-
 @import './assets/styles/main.css';
 @import url('https://fonts.googleapis.com/css2?family=Aldrich&display=swap');
-
-#app {
-  font-family: Aldrich, Helvetica, Avenir, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 </style>
--- frontend/vuejs/src/components/MainScreen.vue
+++ frontend/vuejs/src/components/MainScreen.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="main">
-    <img class="icon" src="@/assets/icon.svg" width="64px" height="64px">
     <template v-if="!sessionFound()">
-      <div class="create-session-container">
-        <button class="button create-button" v-on:click="createSession">
-          create
-        </button>
-        <label>
-          <div class="text">or join by id:</div>
-          <input class="session-id-input" v-model.lazy="sessionId">
-        </label>
+      <div class="grid-container">
+        <div class="grid-item-header">
+          <svg src="" width="64px" height="64px" xmlns="http://www.w3.org/2000/svg"
+               xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:serif="http://www.serif.com/"
+               viewBox="0 0 9 11" version="1.1" xml:space="preserve"
+               style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+            <g id="logo">
+              <path
+                  d="M6.997,0L6.996,-0L8.008,0L8.004,1.004L9,1.002C8.999,2.335 9,5 9,5L8,5L8,6L9,6C9,6 8.999,9.626 9,11L1,11L1,6L0,6L0,5L2,5L2,10L3,10L3,7L4,7L4,6L5,6L5,5L4,5L4,1L5,1L5,0L5.999,0L5.998,2L7.001,2.001C7.001,2.001 7.007,0.068 6.997,0ZM6,3L5,3L5,4L6,4L6,3ZM8,3L7,3L7,4L8,4L8,3Z"/>
+            </g>
+					</svg>
+        </div>
+        <div class="grid-item-main">
+          <button class="button button-big" v-on:click="createSession">create</button>
+          <div class="text text-regular">or</div>
+          <div class="input-container">
+            <input type="text" placeholder="session id..." v-model.lazy="sessionId">
+            <button class="button button-big">join</button>
+          </div>
+          <div style="display: none" class="error text-regular">session with the id is not found</div>
+        </div>
       </div>
     </template>
+
     <template v-if="sessionFound() && userId === ''">
       <label>
         Type name:
@@ -26,6 +39,7 @@
       </label>
       <button v-on:click="joinSession">Join Session</button>
     </template>
+
     <template v-if="sessionFound() && userId !== '' && session !== null">
       <button class='button vote-button' v-on:click="voteInSession(1)">1</button>
       <button class='button vote-button' v-on:click="voteInSession(2)">2</button>
@@ -222,31 +236,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.currentUser {
-  font-weight: bold;
-}
-
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-table {
-  align: left
-}
-</style>
