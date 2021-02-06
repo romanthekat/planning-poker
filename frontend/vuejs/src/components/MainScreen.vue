94b584d39d5241df9c6e9196bed6cafc47b0bd0c
Roman Garanin <git@romangaranin.net>
2021-02-06 17:24:17 +0300

    feat: FE new style for second page

--- frontend/vuejs/src/components/MainScreen.vue
+++ frontend/vuejs/src/components/MainScreen.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="main">
+  <div class="grid-container">
+    <div class="grid-item-header">
+      <svg src="" width="64px" height="64px" xmlns="http://www.w3.org/2000/svg"
+           xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:serif="http://www.serif.com/"
+           viewBox="0 0 9 11" version="1.1" xml:space="preserve"
+           style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+              <g id="logo">
+                <path
+                    d="M6.997,0L6.996,-0L8.008,0L8.004,1.004L9,1.002C8.999,2.335 9,5 9,5L8,5L8,6L9,6C9,6 8.999,9.626 9,11L1,11L1,6L0,6L0,5L2,5L2,10L3,10L3,7L4,7L4,6L5,6L5,5L4,5L4,1L5,1L5,0L5.999,0L5.998,2L7.001,2.001C7.001,2.001 7.007,0.068 6.997,0ZM6,3L5,3L5,4L6,4L6,3ZM8,3L7,3L7,4L8,4L8,3Z"/>
+              </g>
+      </svg>
+    </div>
     <template v-if="!sessionFound()">
-      <div class="grid-container">
-        <div class="grid-item-header">
-          <svg src="" width="64px" height="64px" xmlns="http://www.w3.org/2000/svg"
-               xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:serif="http://www.serif.com/"
-               viewBox="0 0 9 11" version="1.1" xml:space="preserve"
-               style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-            <g id="logo">
-              <path
-                  d="M6.997,0L6.996,-0L8.008,0L8.004,1.004L9,1.002C8.999,2.335 9,5 9,5L8,5L8,6L9,6C9,6 8.999,9.626 9,11L1,11L1,6L0,6L0,5L2,5L2,10L3,10L3,7L4,7L4,6L5,6L5,5L4,5L4,1L5,1L5,0L5.999,0L5.998,2L7.001,2.001C7.001,2.001 7.007,0.068 6.997,0ZM6,3L5,3L5,4L6,4L6,3ZM8,3L7,3L7,4L8,4L8,3Z"/>
-            </g>
-					</svg>
+      <div class="grid-item-main">
+        <button class="button button-big" v-on:click="createSession">create</button>
+        <div class="text text-regular">or</div>
+        <div class="input-container">
+          <input type="text" placeholder="session id..." v-model.lazy="sessionId">
+          <button class="button button-big">join</button>
         </div>
-        <div class="grid-item-main">
-          <button class="button button-big" v-on:click="createSession">create</button>
-          <div class="text text-regular">or</div>
-          <div class="input-container">
-            <input type="text" placeholder="session id..." v-model.lazy="sessionId">
-            <button class="button button-big">join</button>
-          </div>
-          <div style="display: none" class="error text-regular">session with the id is not found</div>
-        </div>
+        <div style="display: none" class="error text-regular">session with the id is not found</div>
       </div>
     </template>
 
     <template v-if="sessionFound() && userId === ''">
-      <label>
-        Type name:
-        <input v-model="name">
-      </label>
-      <button class='button' v-on:click="joinSession">Join Session</button>
+      <div class="grid-item-main">
+        <div class="input-container">
+          <input type="text" placeholder="your name..." v-model="name">
+          <button class='button button-big' v-on:click="joinSession">join</button>
+        </div>
+        <div style="display: none" class="error text-regular">please enter a correct name</div>
+      </div>
     </template>
     <template v-if="sessionFound() && userId !== '' && session === null">
       <label>
